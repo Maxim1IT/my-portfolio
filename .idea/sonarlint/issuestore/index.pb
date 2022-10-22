58801ebdff617cf8a23ed393bb8a6fc8a6d6a2b2
maxim.botnariuc.md <maximbotnariuc1it@gmail.com>
2022-10-22 16:18:57 +0300

    Work in module about me

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,26 @@
+
+D
+public/manifest.json,4\2\42e89cd2213b9d5dfae4f63522702c623170d792
+A
+public/robots.txt,d\a\dac799070c2d0c2fa24462b02c1f2dcc3fb3a1e9
+9
+	yarn.lock,5\3\53585a096fe3a3b96321113856ae62602995a33b
+<
+package.json,7\0\7030d0b2f71b999ff89a343de08c414af32fc93a
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+\
+,.idea/inspectionProfiles/Project_Default.xml,4\9\496a238a6afa168dbaf6efd37bb459331589579c
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+A
+public/index.html,f\4\f43ecdf8d666ee908aacce037b4cd72159e4741b
+I
+src/shared/theme/index.ts,c\9\c9074e9d9c79e8485aee9a587373556f9131ecd1
+=
+src/index.tsx,5\a\5ab44fe33ca96299f51ad4778a73e425b92bf439
+V
+&src/shared/components/Header/index.css,9\4\9422b25134e167d238f1b9fd20cde276ea87d9d9
+V
+&src/shared/components/Header/index.tsx,3\0\3006e4d092e6f5e75790942719496b0f5e0100b0
--- src/modules/about-me/index.tsx
+++ src/modules/about-me/index.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import {Grid, Typography} from "@mui/material";
+import './index.css'
 
 function AboutMe() {
     return (
-        <Grid container>
-            <Grid mt={8} item xs={12} textAlign="center">
-                <Typography sx={{fontWeight:"700"}} variant="h4">Hello, My name is Maxim Botnariuc</Typography>
+        <Grid sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center'
+        }} container>
+            <Grid item mt={8} xs={12} textAlign="center">
+                <Typography sx={{fontWeight: "700"}} variant="h4">HEY, I'M MAXIM BOTNARIUC</Typography>
+            </Grid>
+            <Grid item mt={5} md={6} textAlign="center">
+                <Typography variant="h6">A Frontend focused Web Developer building the Frontend of Websites and Web
+                    Applications that
+                    leads to the success of the overall product</Typography>
+            </Grid>
+            <Grid className='myAvatar' item mt={5}>
+                <img src="https://img.freepik.com/premium-vector/man-face-logo-with-vector_96853-578.jpg" alt="MyPhoto"/>
             </Grid>
         </Grid>
     );
